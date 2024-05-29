28dc4f4d699df23d2a29dcaf383ee39fcf74ec9e
itsonlywhitepowder <146915903+YaBoiFluffie@users.noreply.github.com>
2024-05-29 17:17:54 +0200

    Create AutoAIO.sh

---
+++ AutoAIO.sh
@@ -0,0 +1,21 @@
+# sudo apt install python3-dev python3-venv python3-openai
+mkdir /opt/AUTO_GPT && cd /opt/AUTO_GPT
+# mkdir /opt/python3 && python3 -m venv /opt/python3/bin/activate
+git clone --recursive https://github.com/Significant-Gravitas/AutoGPT
+git clone --recursive https://github.com/Significant-Gravitas/pr-agent-settings
+git clone --recursive https://github.com/Significant-Gravitas/Auto-GPT-Plugins
+git clone --recursive https://github.com/Significant-Gravitas/Auto-GPT-test-cassettes
+git clone --recursive https://github.com/Significant-Gravitas/AutoGPT_Wizard
+git clone --recursive https://github.com/Significant-Gravitas/gravitasml
+git clone --recursive https://github.com/Significant-Gravitas/Nexus
+git clone --recursive https://github.com/Significant-Gravitas/Auto-GPT-Benchmarks
+git clone --recursive https://github.com/Significant-Gravitas/autostandup
+git clone --recursive https://github.com/Significant-Gravitas/vcrpy
+git clone --recursive https://github.com/Significant-Gravitas/opensoft
+git clone --recursive https://github.com/Significant-Gravitas/Auto-GPT-Forge
+git clone --recursive https://github.com/Significant-Gravitas/auto_gpt_flutter_client
+git clone --recursive https://github.com/Significant-Gravitas/postman
+git clone --recursive https://github.com/Significant-Gravitas/Auto-GPT-Plugin-Template
+git clone --recursive https://github.com/Significant-Gravitas/pathfinder
+git clone --recursive https://github.com/Significant-Gravitas/cassettes
+git clone --recursive https://github.com/Significant-Gravitas/Catalysts
